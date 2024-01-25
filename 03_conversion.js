e8b68bb7a62e7c81090af3f90c5d4bc5d1eee6bf
Kshitij_chavan <144587821+ikshitijhere@users.noreply.github.com>
2024-01-25 18:24:06 +0000

    Day_1

--- 03_conversion.js
+++ 03_conversion.js
@@ -11,16 +11,52 @@ let valueinnumber = Number(score)
 //"33abc"=> NAN not a number
 //true => 1 false=> 0
 
-let islogged = "kshitij"
+// let islogged = "kshitij"
 
-let booleanislogged = Boolean(islogged)
-console.log(booleanislogged) 
+// let booleanislogged = Boolean(islogged)
+// console.log(booleanislogged) 
 
 // 1 => true 0 => false
 // "" => false
 // "kshitij" => true
 
-let sumNum = 33
-let strnum = String(sumNum)
-console.log(strnum)
-console.log(typeof strnum)
+// let sumNum = 33
+// let strnum = String(sumNum)
+// console.log(strnum)
+// console.log(typeof strnum)
+
+// ##################### Operations ######################
+
+// let val = 3
+// let negativeval = -val
+// console.log(negativeval)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Hello"
+// let str2 = "Kshitij"
+
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+true)
+
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2 + 2
+
+let gamecount = 100
+gamecount++
+++gamecount
+console.log(gamecount)
