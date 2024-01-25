f9849f342285b1e8c8479399f9b0f4721a313873
Kshitij_chavan <144587821+ikshitijhere@users.noreply.github.com>
2024-01-25 17:50:56 +0000

    Day_01

---
+++ 02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code a newer version
+
+// alert(3+3) //We're using node.js not browser
+
+console.log( 3 + 3 );
+
+// Code redability should be high
+
+console.log("Javascrip")
+
+
+let name = "Kshitij"
+let age = 20
+let islogged = true
+let state;
+
+// number => 2 to power of 53
+// BigInt
+// String => ""
+// boolean => true/false
+// null  standalone value
+// undifined =>not assigned
+// symbol =>to identify uniqueness
+
+// Object
+console.log(typeof undefined) //undifined
+console.log(typeof null) //Object
