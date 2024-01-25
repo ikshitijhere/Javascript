1d841bf53056744e576ef68b96bf4f8a6d01878a
Kshitij_chavan <144587821+ikshitijhere@users.noreply.github.com>
2024-01-25 18:06:06 +0000

    Day_1

---
+++ 03_conversion.js
@@ -0,0 +1,26 @@
+let score = "Kshitij"
+
+// console.log(typeof score)
+
+let valueinnumber = Number(score)
+
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+
+//"22"=> 33
+//"33abc"=> NAN not a number
+//true => 1 false=> 0
+
+let islogged = "kshitij"
+
+let booleanislogged = Boolean(islogged)
+console.log(booleanislogged) 
+
+// 1 => true 0 => false
+// "" => false
+// "kshitij" => true
+
+let sumNum = 33
+let strnum = String(sumNum)
+console.log(strnum)
+console.log(typeof strnum)
