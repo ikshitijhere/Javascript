479514be128e08542e33ee53f84c97a264dc6e9c
Kshitij_chavan <144587821+ikshitijhere@users.noreply.github.com>
2024-01-28 09:31:48 +0000

    Date_time

---
+++ 08_date_time.js
@@ -0,0 +1,17 @@
+// let date = new Date()
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleDateString());
+// console.log(typeof date); //date is a object
+
+// // let newdate = new Date(2024,0,28)// specify year / month by index / and date
+// // console.log(newdate.toLocaleString());
+let newdate1 = new Date("01-28-2024")
+// console.log(newdate1.toLocaleString());
+
+
+let mytimestamp =  Date.now() // its used for comparing
+console.log(mytimestamp);
+console.log(Date.now()/1000); 
+  
+
